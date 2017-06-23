945e6d5201d48fd8f70fa526c544351b0af30890
Lawrence <dinyctis4@gmail.com>
2017-06-23 14:01:07 -0400

    Initial commit

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,15 @@
-def to_rna():
-    pass
+def to_rna(dna):
+    rnadict = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
+        }
+
+    try:
+        transcribed = "".join([rnadict[x] for x in dna])
+
+    except KeyError:
+        transcribed = ""
+
+    return transcribed
