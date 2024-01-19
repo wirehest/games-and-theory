6e0380e20132af1ee893b599638a46a3b9973627
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-01-20 06:04:48 +0900

    Add Consecutive Strings.

--- codewars/k6/consecutive_strings.py
+++ codewars/k6/consecutive_strings.py
@@ -1,58 +1,52 @@
-#!/usr/bin/env python3
-
-"""longest_consec.py
-Consecutive strings
-You are given an array(list) strarr of strings and an integer k. Your 
-task is to return the first longest string consisting of k consecutive 
-strings taken in the array.
-
-Examples:
-strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
-
-Concatenate the consecutive strings of strarr by 2, we get:
-
-treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
-folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
-trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
-blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
-abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
-
-Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
-The first that came is "folingtrashy" so 
-longest_consec(strarr, 2) should return "folingtrashy".
-
-In the same way:
-longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
-n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
-
-Note
-consecutive strings: follow one after another  without an interruption
-
-
-"""
-
-
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
-#logging.disable(logging.CRITICAL)
-
-
 def longest_consec(strarr, k):
     """"""
+    l = len(strarr)
+    strlens = [len(s) for s in strarr]
+    ranges = [tuple(range(i, i+k)) for i in range(l-k+1)]
+    longest = []
 
-    length = len(strarr)
-    logging.info(f"len(strarr): {len(strarr)}")
-
-    #keys = [list(range(i, i + k - 1)) for i in range(len(strarr) - k)]
-    for i in range(len(strarr) - (k - 1)):
+    for indices in ranges:
+        length = 0
+        for i in indices:
+            length += strlens[i]
         
+        longest.append(length)
+
+    try:
+        pos = longest.index(max(longest))
+    except ValueError:
+        return ''
+    else:
+        return ''.join([strarr[x] for x in range(pos, pos+k)])
 
 
-    
-    logging.info(f"keys: {keys}")
-
-strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
-k = 2
-longest_consec(strarr, k)
-
+assert longest_consec(
+    ['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'],
+    2) == 'folingtrashy' or 'abcdefuvwxyz'
+assert longest_consec(
+    ['zone', 'abigail', 'theta', 'form', 'libe', 'zas', 'theta', 'abigail'], 
+    2) == 'abigailtheta'
+assert longest_consec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'],
+    2) == 'abigailtheta'
+assert longest_consec(
+    ['ejjjjmmtthh', 'zxxuueeg', 'aanlljrrrxx', 'dqqqaaabbb', 
+    'oocccffuucccjjjkkkjyyyeehh'], 
+    1) == 'oocccffuucccjjjkkkjyyyeehh'
+assert longest_consec([], 3) == ''
+assert longest_consec(
+    ['itvayloxrp','wkppqsztdkmvcuwvereiupccauycnjutlv',
+    'vweqilsfytihvrzlaodfixoyxvyuyvgpck'], 
+    2) == ('wkppqsztdkmvcuwvereiupccauycnjutl'
+    'vvweqilsfytihvrzlaodfixoyxvyuyvgpck')
+assert longest_consec(
+    ['wlwsasphmxx','owiaxujylentrklctozmymu','wpgozvxxiu'], 
+    2) == 'wlwsasphmxxowiaxujylentrklctozmymu'
+assert longest_consec(
+    ['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], -2) == ''
+assert longest_consec(
+    ['it','wkppv','ixoyx', '3452', 'zzzzzzzzzzzz'], 
+    3) == 'ixoyx3452zzzzzzzzzzzz'
+assert longest_consec(
+    ['it','wkppv','ixoyx', '3452', 'zzzzzzzzzzzz'], 15) == ''
+assert longest_consec(
+    ['it','wkppv','ixoyx', '3452', 'zzzzzzzzzzzz'], 0) == ''
