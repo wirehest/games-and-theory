386bef441407dac6e1611d44d023d3fa8fce3ee6
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-02-09 18:11:47 +0900

    Update formatting.

--- codewars/k5/last-digit-of-a-large-number.js
+++ codewars/k5/last-digit-of-a-large-number.js
@@ -1,22 +1,21 @@
 'use strict';
 
-
 /** Returns the last digit of n**m. */
-function lastDigit(n, m) {  
+function lastDigit(n, m) {
   if (m === 0n) return 1n;
 
   let digits = [];
   for (let i = 1n; i <= 4n; i++) {
-    digits.push((n**i) % 10n);
+    digits.push(n ** i % 10n);
   }
   switch (m % 4n) {
-    case (0n):
+    case 0n:
       return digits[3];
-    case (1n):
+    case 1n:
       return digits[0];
-    case (2n):
+    case 2n:
       return digits[1];
-    case (3n):
+    case 3n:
       return digits[2];
   }
 }
--- codewars/k5/moving-zeroes-to-the-end.js
+++ codewars/k5/moving-zeroes-to-the-end.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
 /** Returns an array with all zeroes moved to the end. */
 function moveZeros(arr) {
-  const newArr = arr.filter(x => x !== 0);
+  const newArr = arr.filter((x) => x !== 0);
   const zeroCount = arr.length - newArr.length;
 
-  return newArr.concat(Array.from({ length: zeroCount }, x => 0));
+  return newArr.concat(Array.from({ length: zeroCount }, (x) => 0));
 }
