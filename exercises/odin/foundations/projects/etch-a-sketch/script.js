730f54854e70f1b9ebc18941a12d5e2d3a8ae68f
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-04-14 21:47:28 +0900

    Add gradient coloring and setSize canceling

--- exercises/odin/foundations/projects/etch-a-sketch/script.js
+++ exercises/odin/foundations/projects/etch-a-sketch/script.js
@@ -12,21 +12,19 @@ function etchASketch() {
       case 'clear':
         clearCells();
         break;
-      case 'color-black':
-      case 'color-gradient':
-      case 'color-random':
-        [, coloring] = targetName.split('-');
-        addDrawingEvent(coloring);
-        // console.log('coloring: ' + coloring);
+      case 'black':
+      case 'gradient':
+      case 'random':
+        addDrawingEvent(targetName);
         break;
     }
   });
 
+  // Prepare initial sketchpad.
   addCells();
   addDrawingEvent(coloring);
 
-  function addCells(width = 50) {
-    // TODO refactor to remove nested loops
+  function addCells(width = 16) {
     for (let i = 0; i < width; i++) {
       let cellRow = document.createElement('div');
       cellRow.className = 'cell-row';
@@ -48,43 +46,59 @@ function etchASketch() {
 
   function addDrawingEvent(coloring = 'black') {
     let cellColor;
+    let gradientAlpha = 0;
+    let gradientDelta = 0.1;
+    let gradientIncreasing = true;
+
     container.addEventListener('mouseover', (event) => {
       if (event.target.className === 'cell') {
-        if (coloring === 'black') cellColor = 'rgba(0, 0, 0, 1)';
-        if (coloring === 'random') {
-          let rgba = { r: 0, g: 0, b: 0 };
-          for (let channel in rgba) {
-            rgba[channel] = Math.floor(Math.random() * 256);
-          }
-          cellColor = `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, 1)`;
+        switch (coloring) {
+          case 'black':
+            cellColor = makeRgbaString(0, 0, 0, 1);
+            break;
+          case 'gradient':
+            if (gradientIncreasing) {
+              if (gradientAlpha < 1) gradientAlpha += gradientDelta;
+              else gradientIncreasing = false;
+            } else {
+              if (gradientAlpha > 0) gradientAlpha -= gradientDelta;
+              else gradientIncreasing = true;
+            }
+            cellColor = makeRgbaString(0, 0, 0, gradientAlpha);
+            break;
+          case 'random':
+            let rgb = { r: 0, g: 0, b: 0 };
+            for (let channel in rgb) {
+              rgb[channel] = Math.floor(Math.random() * 256);
+            }
+            cellColor = makeRgbaString(rgb.r, rgb.g, rgb.b, 1);
+            break;
         }
+
         event.target.setAttribute('style', `background-color: ${cellColor}`);
-        // event.target.classList.toggle('shaded');
       }
     });
+
+    function makeRgbaString(red, green, blue, alpha) {
+      return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    }
   }
 
   function setSize() {
-    let gridWidth = -Infinity;
-    while (!(gridWidth >= 1 && gridWidth <= 100)) {
-      gridWidth = Math.round(+prompt('Enter grid width (between 1 and 100):'));
+    let gridWidth;
+
+    while (true) {
+      gridWidth = prompt('Enter grid width (between 1 and 100):');
+      if (gridWidth === null || gridWidth === '') return;
+      gridWidth = Math.round(gridWidth);
+      if (!(gridWidth >= 1 && gridWidth <= 100)) continue;
+      break;
     }
 
     let cellRows = document.querySelectorAll('.cell-row');
     cellRows.forEach((cellRow) => cellRow.remove());
     addCells(gridWidth);
   }
-
-  // function setColor(color = 'black') {
-  //   switch (color) {
-  //     case 'black':
-  //       break;
-  //     case 'gradient':
-  //       break;
-  //     case 'random':
-  //       break;
-  //   }
-  // }
 }
 
 etchASketch();
