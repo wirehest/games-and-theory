e5ddc54fd7bc086a2233b1743ad5c7ce74e633b8
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-04-19 22:29:41 +0900

    Redo event listener callback logic
    This changes the main event listener callback logic from switch to
    if...else statements for clarity.

--- exercises/odin/foundations/projects/calculator/script.js
+++ exercises/odin/foundations/projects/calculator/script.js
@@ -1,56 +1,108 @@
 function mainLoop() {
   let display = document.querySelector('.display');
   let container = document.querySelector('.container');
-  // let firstNumber;
-  // let secondNumber;
+  let operator = null;
+  let num1 = null;
+  let num2 = null;
+  let decimal = false;
 
-  // let buttons = document.querySelectorAll('button');
-  // buttons.forEach((button) =>
-  //   button.addEventListener('click', (event) => console.log(event))
-  // );
+  let startChain = false;
+  let clearOnNumber = false;
+  let clearDisplay = () => (display.textContent = '');
+
+  // console.log(operate({ operation: '+', a: '1', b: '2' }));
 
   container.addEventListener('click', (event) => {
-    if (event.target.nodeName === 'BUTTON') {
-      switch (event.target.textContent) {
-        case 'AC':
-          display.textContent = '';
-          break;
-        case '=':
-          let argsObject = parseOperation(display.textContent);
-          // console.log(argsObject);
-          // console.log(operate(argsObject));
-          display.textContent = operate(argsObject);
-          break;
-        default:
-          display.textContent += event.target.textContent;
+    if (event.target.nodeName !== 'BUTTON') return;
+
+    let button = event.target.textContent;
+    let buttonIsNumber = '.0123456789'.includes(button);
+    let buttonIsOperator = '/*-+'.includes(button);
+    let buttonIsEquals = button === '=';
+    let displayHasNumber = display.textContent !== '';
+    let num1AndOperatorNotNull = !(num1 === null) && !(operator === null);
+
+    if (button === 'AC') {
+      clearDisplay();
+      [operator, num1, num2, clearOnNumber] = [null, null, null, true];
+    }
+
+    if (buttonIsNumber) {
+      if (clearOnNumber) clearDisplay();
+      clearOnNumber = false;
+
+      if (display.textContent.includes('.') && button === '.') return;
+      if (display.textContent === '0' && button === '0') return;
+      if (display.textContent === '0' && !(button === '.')) {
+        display.textContent = button;
+        return;
+      }
+      if (display.textContent === '' && button === '.') {
+        display.textContent = '0.';
+        return;
+      }
+      display.textContent += button;
+    }
+
+    if (buttonIsOperator) {
+      if (num1AndOperatorNotNull) {
+        if (displayHasNumber) {
+          clearOnNumber = true;
+          num2 = display.textContent;
+          num1 = operate({ operator, num1, num2 });
+          display.textContent = num1;
+          num2 = null;
+          operator = button;
+        } else {
+          operator = button;
+        }
+      } else {
+        if (displayHasNumber) {
+          clearOnNumber = true;
+          num1 = display.textContent;
+          operator = button;
+        }
+      }
+    }
+
+    if (buttonIsEquals) {
+      if (num1AndOperatorNotNull) {
+        if (displayHasNumber) {
+          clearOnNumber = true;
+          num2 = display.textContent;
+          num1 = operate({ operator, num1, num2 });
+          display.textContent = num1;
+          [num2, operator] = [null, null];
+        }
       }
     }
   });
 
-  function operate({ operator, a, b }) {
-    const add = (a, b) => a + b;
-    const subtract = (a, b) => a - b;
-    const multiply = (a, b) => a * b;
-    const divide = (a, b) => a / b;
+  function operate({ operator, num1, num2 }) {
+    [num1, num2] = [+num1, +num2];
+    const add = (num1, num2) => num1 + num2;
+    const subtract = (num1, num2) => num1 - num2;
+    const multiply = (num1, num2) => num1 * num2;
+    const divide = (num1, num2) => (num2 === 0 ? 'ERROR' : num1 / num2);
 
     switch (operator) {
       case '+':
-        return add(a, b);
+        return add(num1, num2);
       case '-':
-        return subtract(a, b);
+        return subtract(num1, num2);
       case '*':
-        return multiply(a, b);
+        return multiply(num1, num2);
       case '/':
-        return divide(a, b);
+        return divide(num1, num2);
     }
   }
 
-  function parseOperation(displayString) {
-    let operatorsPattern = /[\+\-\*\/]/gu;
-    let operator = displayString.match(operatorsPattern)[0];
-    let [a, b] = displayString.split(operatorsPattern).map((x) => +x);
-    return { operator, a, b };
-  }
+  // function parseOperation(displayString) {
+  //   let operatorsPattern = /[\+\-\*\/]/gu;
+  //   let operator = displayString.match(operatorsPattern)[0];
+  //   let [a, b] = displayString.split(operatorsPattern).map((x) => +x);
+  //   return { operator, a, b };
+  // }
 }
 
 mainLoop();
@@ -62,9 +114,15 @@ mainLoop();
 // You should round answers with long decimals so that they don’t overflow the screen.
 // Pressing = before entering all of the numbers or an operator could cause problems!
 // Pressing “clear” should wipe out any existing data. Make sure the user is really starting fresh after pressing “clear”
-// Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
 
-// Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5. It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)
-// Make it look nice! This is a great project to practice your CSS skills. At least make the operations a different color from the keypad buttons.
+// Users can get floating point numbers if they do the math required to get one,
+// but they can’t type them in yet. Add a . button and let users input decimals!
+// Make sure you don’t let them type more than one though: 12.3.56.5. It is hard
+// to do math on these numbers. (disable the decimal button if there’s already one
+// in the display)
+
+// At least make the operations a different color from the keypad buttons.
+
 // Add a “backspace” button, so the user can undo if they click the wrong number.
+
 // Add keyboard support!
