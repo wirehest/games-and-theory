07f3702129748d88176d4bcaab1b97fb5f79b5e8
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-02 05:45:12 +0900

    Update structure and styling
    Add background image, logo, and font assets.
    Add styling to mimic project sample.
    Add html structure to mimic project sample.
    Start function for password-checking.

--- exercises/odin/full-stack-javascript/01-intermediate-html-css/projects/sign-up-form/script.js
+++ exercises/odin/full-stack-javascript/01-intermediate-html-css/projects/sign-up-form/script.js
@@ -1,1 +1,13 @@
-// TODO  for verifying identical passwords
+function checkPasswordsMatch() {
+  let password = document.querySelector('#pass').value;
+  let confirmPass = document.querySelector('#confirmpass').value;
+  let createButton = document.querySelector('.button-create-account');
+
+  createButton.addEventListener('click', () => {
+    // console.log(password, confirmPass);
+    if (password.value === confirmPass.value) return;
+    // password.setCustomValidity
+  });
+}
+
+checkPasswordsMatch();
