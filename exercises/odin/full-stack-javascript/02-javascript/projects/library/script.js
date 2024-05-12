613bfd2f8e4cad67e799a8efff577d11a8c8b0be
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-13 06:14:34 +0900

    Add basic bookshelf structure and early functions
    Add displayBooks() which inserts books into the bookshelf
    Add basic styling

--- exercises/odin/full-stack-javascript/02-javascript/projects/library/script.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/library/script.js
@@ -12,18 +12,34 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read)); 
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 function displayBooks() {
-  let bookshelf = document.querySelector('');
+  let bookshelf = document.querySelector('.bookshelf');
+
+  while (myLibrary.length > 0) {
+    let libEntry = myLibrary.pop();
+    let book = document.createElement('div');
+    book.classList.add('book', 'shadow');
+
+    ['title', 'author'].forEach((x) => {
+      let detail = document.createElement('div');
+      detail.classList.toggle(x);
+      detail.textContent = libEntry[x];
+      book.appendChild(detail);
+    });
+
+    bookshelf.appendChild(book);
+  }
 }
 
-
-
-// let myBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-// console.log(myBook.info());
-// 
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('Farewell My Lovely', 'Raymond Chandler', 306, true);
 // console.log(myLibrary);
-// addBookToLibrary('Farewell My Lovely', 'Raymond Chandler', 306, true);
-// console.log(myLibrary);
+displayBooks();
