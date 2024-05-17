3277fef6b930daf9e565d057cb3253fd0c4f276e
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-18 05:00:35 +0900

    Initial commit: Tic Tac Toe project

---
+++ exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
@@ -0,0 +1,13 @@
+// TODO store gameboard as array inside Gameboard object
+// TODO store players in objects
+// TODO game flow control via object
+// TODO render object
+// TODO functions to allow interaction with the DOM (also checks if cell
+// already occupied
+// TODO setup functionality incl. add player names, start/restart, display
+// game results
+//
+// minimize global code
+// use factory functions and IIFEs where necessary
+// 
+// allow for tie games
