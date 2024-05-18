4890387118f0f1cd241d39f76f6fafb58b53410d
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-19 06:44:40 +0900

    Add factory func. for game board

--- exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
@@ -9,5 +9,41 @@
 //
 // minimize global code
 // use factory functions and IIFEs where necessary
-// 
-// allow for tie games
+//
+// allow for tie
+
+(function () {
+  let board = (function () {
+    let grid = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    let markGrid = (x, y, mark) => {
+      if (grid[x][y] !== null) throw new Error('cell already marked');
+      grid[x][y] = mark;
+    };
+    let checkGrid = () => {
+      let gameWon = false;
+      // [0, 1, 2].forEach((x) => {
+      // });
+    };
+    let printGrid = () => console.log(grid);
+    return { markGrid, printGrid };
+  })();
+
+  function player(name, mark) {
+    return { name, mark };
+  }
+
+  function gameController(player1, player2) {
+    // TODO add player names
+    // TODO when game end, display results
+    let marks = { X: true, O: true };
+  }
+
+  // board.printGrid();
+  // board.markGrid(0, 0, 'X');
+  // board.printGrid();
+  // board.markGrid(0, 0, 'O');
+})();
