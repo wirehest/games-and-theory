71f1db7d88fd7e3ee7d9f56a1c056823b02a01ed
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-19 23:54:40 +0900

    Add basic game loop

--- exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
@@ -12,6 +12,10 @@
 //
 // allow for tie
 
+// readline for user input in console
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
 (function () {
   let board = (function () {
     let grid = [
@@ -19,31 +23,93 @@
       [null, null, null],
       [null, null, null],
     ];
+
     let markGrid = (x, y, mark) => {
       if (grid[x][y] !== null) throw new Error('cell already marked');
       grid[x][y] = mark;
     };
+
     let checkGrid = () => {
       let gameWon = false;
-      // [0, 1, 2].forEach((x) => {
-      // });
+      // check rows
+      [0, 1, 2].forEach((x) => {
+        if (
+          grid[x][0] !== null &&
+          grid[x][0] === grid[x][1] &&
+          grid[x][1] === grid[x][2]
+        ) {
+          gameWon = !gameWon;
+        }
+      });
+
+      // check columns
+      [0, 1, 2].forEach((y) => {
+        if (
+          grid[0][y] !== null &&
+          grid[0][y] === grid[1][y] &&
+          grid[1][y] === grid[2][y]
+        ) {
+          gameWon = !gameWon;
+        }
+      });
+
+      // check diagonals
+      if (grid[1][1] !== null) {
+        if (
+          (grid[1][1] === grid[0][2] && grid[1][1] === grid[2][0]) ||
+          (grid[1][1] === grid[0][0] && grid[1][1] === grid[2][2])
+        ) {
+          gameWon = !gameWon;
+        }
+      }
+
+      return gameWon;
     };
+
     let printGrid = () => console.log(grid);
-    return { markGrid, printGrid };
+
+    return { markGrid, printGrid, checkGrid };
   })();
 
   function player(name, mark) {
-    return { name, mark };
+    let moves = 0;
+    let addMove = () => moves++;
+    let printMoves = () => moves;
+    return { name, mark, addMove, printMoves };
   }
 
-  function gameController(player1, player2) {
+  async function gameController() {
     // TODO add player names
+    // let marks = { X: true, O: true };
+
+    const rl = readline.createInterface({ input, output });
+
+    let player1 = player('Albert', 'X');
+    let player2 = player('Betty', 'O');
+    let position = '';
+    let currentPlayer = player1;
+    let [x, y] = [null, null];
+
+    console.log(
+      'Enter row and column positions separated by a comma, e.g., "0,1".'
+    );
+    while (true) {
+      board.printGrid();
+      position = await rl.question(`${currentPlayer.name}, your turn: `);
+      [x, y] = position.split(',');
+      board.markGrid(+x, +y, currentPlayer.mark);
+      currentPlayer.addMove();
+      if (board.checkGrid()) break;
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    board.printGrid();
+    console.log(
+      currentPlayer.name + ' won in ' + currentPlayer.printMoves() + ' moves.'
+    );
+    rl.close();
     // TODO when game end, display results
-    let marks = { X: true, O: true };
   }
 
-  // board.printGrid();
-  // board.markGrid(0, 0, 'X');
-  // board.printGrid();
-  // board.markGrid(0, 0, 'O');
+  gameController();
 })();
