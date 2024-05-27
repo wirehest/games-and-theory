237e4e4c8a9e2693ffa0f32080384fb1cecae8c9
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-28 06:53:53 +0900

    Refactor JS
    - Change checkForWin() to use fixed array of winning combos
    for simplicity.
    - Move player creation into gameController().
    - Consolidate all multiple DOM-updating functions as
      sub-functions in updateBoard().

--- exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/tic-tac-toe/script.js
@@ -1,295 +1,278 @@
+// import * as readline from 'node:readline/promises';
+// import { stdin as input, stdout as output } from 'node:process';
+
 let grid = (function () {
-  let moves = 0;
+  let marks = 0;
   let cells = new Array(9).fill(null);
 
-  let countMoves = () => cells.filter((cell) => cell !== null).length;
+  let countMarks = () => cells.filter((cell) => cell !== null).length;
   let getCells = () => cells;
+
+  let resetCells = () => {
+    cells.fill(null);
+    moves = 0;
+  };
 
   let markCell = (i, mark) => {
     if (cells[i] !== null) throw new Error('cell already marked');
     cells[i] = mark;
-    moves++;
+    marks++;
   };
 
-  let resetCells = () => {
-    grid.fill(null);
-    moves = 0;
-  };
-
+  // returns undefined if no winner,
+  // array of winning indices if winner
   let checkForWin = () => {
-    // check rows
-    for (let x of [0, 3, 6]) {
-      if (
-        cells[0 + x] !== null &&
-        cells[0 + x] === cells[1 + x] &&
-        cells[0 + x] === cells[2 + x]
-      ) {
-        return [0, 1, 2].map((index) => index + x);
+    let winners = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let winner of winners) {
+      let [a, b, c] = winner;
+      if (cells[a] !== null && cells[a] === cells[b] && cells[a] === cells[c]) {
+        return winner;
       }
-    }
-    // check columns
-    for (let y of [0, 1, 2]) {
-      if (
-        cells[0 + y] !== null &&
-        cells[0 + y] === cells[3 + y] &&
-        cells[0 + y] === cells[6 + y]
-      ) {
-        return [0, 3, 6].map((index) => index + y);
-      }
-    }
-    // check diagonals
-    if (cells[4] !== null) {
-      if (cells[4] === cells[0] && cells[4] === cells[8]) return [0, 4, 8];
-      if (cells[4] === cells[2] && cells[4] === cells[6]) return [2, 4, 6];
     }
   };
 
-  return { countMoves, getCells, markCell, resetCells, checkForWin };
+  return { countMarks, getCells, resetCells, markCell, checkForWin };
 })();
 
-function makePlayers(n1 = 'P1', n2 = 'P2') {
-  let p1 = makePlayer(n1, 'x');
-  let p2 = makePlayer(n2, 'o');
-  let current = p1;
+function makePlayer(name, mark) {
+  let [moves, wins] = [0, 0];
 
-  let getCurrent = () => current;
-  let flipTurn = () => (current = current === p1 ? p2 : p1);
-  let resetMoves = () => [p1, p2].forEach((p) => p.resetMoves());
-  // let resetMovesAndWins = () => [p1, p2].forEach((p) => p.resetAll());
+  let getName = () => name;
+  let getMark = () => mark.toLowerCase();
+  let getWins = () => String(wins).padStart(3, '0');
+  let getMoves = () => moves;
+  let addWin = () => wins++;
+  let addMove = () => moves++;
+  let resetMoves = () => (moves = 0);
 
-  function makePlayer(name, mark) {
-    let [moves, wins] = [0, 0];
-
-    let getName = () => name;
-    let getMark = () => mark.toLowerCase();
-    let getWins = () => String(wins).padStart(3, '0');
-    let getMoves = () => moves;
-    let addWin = () => wins++;
-    let addMove = () => moves++;
-    let resetMoves = () => (moves = 0);
-    // let resetAll = () => ([moves, wins] = [0, 0]);
-
-    return {
-      getName,
-      getMark,
-      getWins,
-      getMoves,
-      addWin,
-      addMove,
-      resetMoves,
-      // resetAll,
-    };
-  }
-
-  return { p1, p2, getCurrent, flipTurn, resetMoves }; // , resetMovesAndWins };
+  return {
+    getName,
+    getMark,
+    getWins,
+    getMoves,
+    addWin,
+    addMove,
+    resetMoves,
+  };
 }
 
 function gameController() {
-  let [players, currentPlayer, state] = [null, null, null];
+  let [p1, p2] = makePlayers();
+  let currentPlayer = p1;
+  // let active = true;
 
-  let start = (p1Name, p2Name) => {
-    if (state === 'playing') return;
-    if (players === null) {
-      players = makePlayers(p1Name, p2Name);
-      currentPlayer = players.getCurrent();
-    }
-    state = 'playing';
-  };
+  function getPlayers() {
+    return {
+      p1Name: p1.getName(),
+      p1Wins: p1.getWins(),
+      p2Name: p2.getName(),
+      p2Wins: p2.getWins(),
+    };
+  }
+
+  function makePlayers(p1Name = 'P1', p2Name = 'P2') {
+    return [makePlayer(p1Name, 'x'), makePlayer(p2Name, 'o')];
+  }
 
   let restart = () => {
     grid.resetCells();
-    players.resetMoves();
-    // no change to state variable
+    [p1, p2].forEach((p) => p.resetMoves());
   };
 
   let reset = () => {
     grid.resetCells();
-    players = makePlayers(players.p1.getName(), players.p2.getName());
-    state = null;
+    [p1, p2] = makePlayers();
   };
+
+  // let getState = () => state;
+  // let setStateNull = () => (state = null);
+
+  let flipTurn = () => (currentPlayer = currentPlayer === p1 ? p2 : p1);
 
   let playTurn = (i) => {
     grid.markCell(i, currentPlayer.getMark());
     currentPlayer.addMove();
 
-    if (grid.countMoves() >= 5) {
+    if (grid.countMarks() >= 5) {
       if (grid.checkForWin() !== undefined) {
+        let winner = currentPlayer.getName();
+        let moves = currentPlayer.getMoves();
         currentPlayer.addWin();
-        state = 'won';
-        return grid.checkForWin();
+        return [`${winner} won in ${moves} moves!`, grid.checkForWin()];
       }
 
-      if (grid.countMoves() === 9) {
-        state = 'tied';
-        return;
+      if (grid.countMarks() === 9) {
+        return ['You tied!'];
       }
     }
-    players.flipTurn();
-    currentPlayer = players.getCurrent();
+
+    flipTurn();
   };
 
-  let getState = () => state;
-  let getPlayers = () => players;
-
-  return { start, restart, reset, playTurn, getState, getPlayers };
+  return { getPlayers, makePlayers, restart, reset, flipTurn, playTurn };
 }
 
-(function displayController() {
+function displayController() {
   let game = gameController();
-  let players = game.getPlayers();
-
-  let p1Box = document.querySelector('.p1');
-  let p2Box = document.querySelector('.p2');
+  let turnResult = null;
   let container = document.querySelector('.container');
-  let domCells = document.querySelectorAll('.gameboard > .cell');
-
-  function clearBoard() {
-    domCells.forEach((domCell) => domCell.classList.remove('x', 'o'));
-    [p1Box, p2Box].forEach((player) =>
-      player.classList.remove('turn-highlight'),
-    );
-  }
+  let resultModal = document.querySelector('.modal.result');
 
   function updateBoard() {
-    grid.getCells().forEach((cell, i) => {
-      if (cell === null) return;
-      domCells[i].classList.add(cell);
-    });
+    let domCells = document.querySelectorAll('.gameboard > .cell');
+    let p = game.getPlayers();
+    let [p1Box, p2Box] = document.querySelectorAll('.p1, .p2');
 
-    if (players.getCurrent() === players.p1) {
-      p1Box.classList.add('turn-highlight');
-      p2Box.classList.remove('turn-highlight');
-    } else {
-      p2Box.classList.add('turn-highlight');
-      p1Box.classList.remove('turn-highlight');
-    }
+    (function clearDomCells() {
+      domCells.forEach((domCell) => domCell.classList.remove('x', 'o'));
+      [p1Box, p2Box].forEach((p) => p.classList.remove('turn-highlight'));
+    })();
+
+    (function markDomCells() {
+      grid.getCells().forEach((cell, i) => {
+        if (cell === null) return;
+        domCells[i].classList.add(cell);
+      });
+    })();
+
+    (function updateScores() {
+      let [p1Wins, p2Wins] = document.querySelectorAll('.p1-score, .p2-score');
+      [p1Wins.textContent, p2Wins.textContent] = [p.p1Wins, p.p2Wins];
+      // p1Wins.textContent = p.p1Wins;
+    })();
+
+    (function updateNames() {
+      let [p1Name, p2Name] = document.querySelectorAll('.p1-name, .p2-name');
+      [p1Name.textContent, p2Name.textContent] = [p.p1Name, p.p2Name];
+      // p1Name.textContent = p.p1Name;
+    })();
+
+    // TODO highlight winning cells (use turnResult array)
+
+    // highlights the current player's box to indicate turn
+    // if (players.getCurrent() === players.p1) {
+    //   p1Box.classList.add('turn-highlight');
+    //   p2Box.classList.remove('turn-highlight');
+    // } else {
+    //   p2Box.classList.add('turn-highlight');
+    //   p1Box.classList.remove('turn-highlight');
+    // }
   }
 
-  function updatePlayerBoxes() {
-    let [p1Name, p1Mark] = p1Box.querySelectorAll('.p1-name, .p1-mark');
-    let [p2Name, p2Mark] = p2Box.querySelectorAll('.p2-name, .p2-mark');
-    p1Name.textContent = players.p1.getName();
-    p2Name.textContent = players.p2.getName();
-    p1Mark.classList.add(String(players.p1.getMark()));
-    p2Mark.classList.add(String(players.p2.getMark()));
-  }
+  // handles marking the grid and advancing the game
+  function addGameListener() {
+    container.addEventListener('click', markListener);
 
-  function updateScores() {
-    let p1Score = document.querySelector('.p1-score');
-    let p2Score = document.querySelector('.p2-score');
-    p1Score.textContent = players.p1.getWins();
-    p2Score.textContent = players.p2.getWins();
-  }
-
-  let addGameBoardListener = () => {
-    let markListener = (event) => {
+    function markListener(event) {
       let target = event.target;
 
       if (target.className.startsWith('cell')) {
-        game.playTurn(target.dataset.pos);
+        turnResult = game.playTurn(target.dataset.pos);
         updateBoard();
 
-        if (['won', 'includes'].includes(game.getState())) {
-          let message = document.querySelector('.modal.result .message');
-
-          switch (game.getState()) {
-            case 'won':
-              let winner = game.getPlayers().getCurrent();
-              let name = winner.getName();
-              let moves = winner.getMoves();
-
-              updateScores();
-              message.textContent = `${name} won in ${moves} moves!`;
-              break;
-            case 'tied':
-              message.textContent = 'You tied!';
-              break;
-          }
-          document.querySelector('.modal.result').showModal();
+        if (turnResult) {
+          let message = resultModal.querySelector('.message');
+          message.textContent = turnResult[0];
+          resultModal.showModal();
           container.removeEventListener('click', markListener);
         }
       }
-    };
-    container.addEventListener('click', markListener);
-  };
+    }
+  }
 
-  (function addButtonListener() {
+  (function addNavListener() {
     container.addEventListener('click', (event) => {
       let target = event.target;
-      let playerEntry = document.querySelector('.modal.player-entry');
-      // let playerDetails;
+      let playersModal = document.querySelector('.modal.player-entry');
+      let playersForm = playersModal.querySelector('form');
+      let resultModal = document.querySelector('.modal.result');
+      let isFirstGame = true;
 
-      switch (target.className) {
-        case 'play':
-          if (players === null) {
-            playerEntry.showModal();
-          } else {
-            clearBoard();
-            game.start();
-            players = game.getPlayers();
-            updatePlayerBoxes();
-            addGameBoardListener();
-          }
-          break;
-        case 'start':
-          playerEntry.close();
-          let { p1Name = 'P1', p2Name = 'P2' } = Object.fromEntries(
-            new FormData(playerEntry.querySelector('form')),
-          );
-          // console.log(playerData);
-          // console.log('275', p1Name, p2Name);
-          game.start(p1Name, p2Name);
-          players = game.getPlayers();
-          updatePlayerBoxes();
-          addGameBoardListener();
-          break;
+      // main buttons
+      if (target.className === 'play') {
+        if (isFirstGame) {
+          playersModal.showModal();
+          isFirstGame = false;
+        } else addGameListener();
       }
 
-      if (target.className === 'play-again') {
-        target.parentElement.parentElement.close();
-        clearBoard();
-        game.start();
-        // game.getPlayers().randomizeStartingPlayer();
-        updatePlayerBoxes();
-        addGameBoardListener();
+      if (target.className === 'restart') {
+        game.restart();
+        updateBoard();
+        addGameListener();
       }
 
       if (target.className === 'reset') {
-        clearBoard();
         game.reset();
-        game.start();
-        players = game.getPlayers();
-        // game.getPlayers().randomizeStartingPlayer();
-        updateScores();
-        updatePlayerBoxes();
-        addGameBoardListener();
+        isFirstGame = true;
+        updateBoard();
       }
 
-      if (target.className === 'close') {
-        target.parentElement.parentElement.close();
+      // modal buttons
+      if (target.className === 'start') {
+        let { p1Name, p2Name } = Object.fromEntries(new FormData(playersForm));
+        console.log(p1Name, p2Name);
+        game.makePlayers(p1Name, p2Name);
+        console.log(game.getPlayers());
+        playersModal.close();
+        playersForm.reset();
+
+        updateBoard();
+        addGameListener();
       }
+
+      if (target.className === 'play-again') {
+        game.restart();
+        updateBoard();
+        addGameListener();
+        resultModal.close();
+      }
+
+      if (target.className === 'close') resultModal.close();
+      if (target.className === 'cancel') playersModal.close();
+      // common statements
+      // game.setStateNull();
+      // players = game.getPlayers();
+      // clearBoard();
+      // updatePlayerBoxes();
+      // addGameBoardListener();
     });
   })();
-})();
-
-// displayController();
+}
+displayController();
 
 // console-only gameloop
-// let rl = readline.createInterface({ input, output });
-// let game = gameController();
+// const rl = readline.createInterface({ input, output });
 //
+// let game = gameController();
 // let name1 = await rl.question('Enter name for player 1: ');
-// let mark1 = await rl.question('Player 1, select your mark (X or O): ');
 // let name2 = await rl.question('Enter name for player 2: ');
 //
-// game.startGame(name1, mark1, name2);
-// board.printGrid();
-// while (game.getActive()) {
+// game.play(name1, name2);
+// printGrid();
+// while (game.getState()) {
 //   let position = await rl.question(
-//     `${game.getPlayers().getCurrent().getName()}, your turn: `
+//     `${game.getPlayers().getCurrent().getName()}, your turn: `,
 //   );
-//   let [x, y] = position.split(',');
-//   game.playTurn(+x, +y);
+//   game.playTurn(+position);
+//   printGrid();
 // }
 // game.gamesPlayed++;
+//
 // rl.close();
+//
+// function printGrid() {
+//   console.log(grid.getCells().slice(0, 3));
+//   console.log(grid.getCells().slice(3, 6));
+//   console.log(grid.getCells().slice(6, 9));
+// }
