e231b1ca78fabd51d9cabb5d1c1b6b31cfa8cf6d
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-05-31 03:31:05 +0900

    Refactor factory functions with classes
    - Replace Book factory function with Book class, and add new Library
    class for the bookshelf.
    - Turn addBookToLibrary() into a method in the Library class, and add
    other methods.
    - Remove main() loop wrapping the entire script because it is
    unnecessary.
    - Add addMainWindowListeners() function that adds event listeners for
    navigation in the main window. Currently, only the 'New Book' button.

--- exercises/odin/full-stack-javascript/02-javascript/projects/library/script.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/library/script.js
@@ -1,105 +1,132 @@
-function main() {
-  let myLibrary = [];
-
-  function Book(title, author, pages, read) {
+class Book {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function () {
-      return `${title} by ${author}. ${pages} pages. ${read}.`;
-    };
   }
 
-  function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+  flipReadStatus() {
+    this.read != this.read;
+  }
+}
+
+class Library {
+  #library = [];
+
+  addBook(title, author, pages, read) {
+    this.#library.push(new Book(title, author, pages, read));
+    return this.#library;
   }
 
-  function clearBooks() {
-    let books = document.querySelectorAll('.book');
-    books.forEach((book) => book.remove());
+  removeBook(index) {
+    this.#library.splice(index, 1);
+    return this.#library;
   }
 
-  function displayBooks() {
-    // displayBooks redraws all books, so need to clear bookshelf first
-    clearBooks();
+  clearLibrary() {
+    this.#library = [];
+    return this.#library;
+  }
 
-    let bookshelf = document.querySelector('.bookshelf');
-    let libIndex = 0;
+  get books() {
+    return this.#library;
+  }
+}
 
-    myLibrary.forEach((libEntry) => {
-      let book = document.createElement('div');
-      book.classList.add('book', 'shadow');
-      book.setAttribute('data-libindex', libIndex++);
+let myLibrary = new Library();
 
-      // adds delete button to book
-      let delButton = document.createElement('button');
-      delButton.type = 'button';
-      delButton.textContent = '✖';
-      delButton.classList.toggle('delete-button');
-      book.appendChild(delButton);
+function clearBooks() {
+  let books = document.querySelectorAll('.book');
+  books.forEach((book) => book.remove());
+}
 
-      // adds title, author, pages, and read details from libEntry to book
-      ['title', 'author', 'pages'].forEach((bookDetail) => {
-        let detail = document.createElement('div');
-        detail.classList.toggle(bookDetail);
-        detail.textContent = libEntry[bookDetail];
-        if (bookDetail === 'pages') detail.textContent += ' pages';
-        book.appendChild(detail);
-      });
+function displayBooks() {
+  // displayBooks redraws all books, so need to clear bookshelf first
+  clearBooks();
 
-      // adds toggle functionality to the read status
-      let read = document.createElement('div');
-      read.classList.toggle('read');
-      read.textContent = libEntry['read'] ? 'Read' : 'Unread';
-      book.appendChild(read);
+  let bookshelf = document.querySelector('.bookshelf');
+  let libIndex = 0;
 
-      book.addEventListener('click', (event) => {
-        let bookIndex = event.target.parentNode.attributes[1].value;
-        if (event.target.className === 'read') {
-          myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
-        }
-        if (event.target.className === 'delete-button') {
-          myLibrary.splice(bookIndex, 1);
-        }
-        displayBooks();
-      });
-      bookshelf.appendChild(book);
+  myLibrary['books'].forEach((libEntry) => {
+    let book = document.createElement('div');
+    book.classList.add('book', 'shadow');
+    book.setAttribute('data-libindex', libIndex++);
+
+    // adds delete button to book
+    let delButton = document.createElement('button');
+    delButton.type = 'button';
+    delButton.textContent = '✖';
+    delButton.classList.toggle('delete-button');
+    book.appendChild(delButton);
+
+    // adds title, author, pages, and read details from libEntry to book
+    ['title', 'author', 'pages'].forEach((bookDetail) => {
+      let detail = document.createElement('div');
+      detail.classList.toggle(bookDetail);
+      detail.textContent = libEntry[bookDetail];
+      if (bookDetail === 'pages') detail.textContent += ' pages';
+      book.appendChild(detail);
     });
-    console.log(myLibrary);
-  }
 
-  // sets event listeners related to the 'add new book' modal
-  function setModalControls() {
-    let modal = document.querySelector('.add-book-modal');
-    let form = document.querySelector('.add-book-form');
+    // adds toggle functionality to the read status
+    let read = document.createElement('div');
+    read.classList.toggle('read');
+    read.textContent = libEntry['read'] ? 'Read' : 'Unread';
+    book.appendChild(read);
 
-    document.addEventListener('click', (event) => {
-      let button = event.target.className;
-      if (button === 'new-book-button') modal.showModal();
-      if (button === 'close-button') {
-        form.reset();
-        modal.close();
+    book.addEventListener('click', (event) => {
+      let bookIndex = event.target.parentNode.attributes[1].value;
+      if (event.target.className === 'read') {
+        myLibrary['books'][bookIndex].read =
+          !myLibrary['books'][bookIndex].read;
       }
-    });
-
-    // submit event is triggered by submit buttons when
-    // the associated form method is 'dialog'
-    document.addEventListener('submit', (event) => {
-      let title = document.querySelector('#title').value;
-      let author = document.querySelector('#author').value;
-      let pages = +document.querySelector('#pages').value;
-      let read = document.querySelector('#read').checked;
-      addBookToLibrary(title, author, pages, read);
-      form.reset();
+      if (event.target.className === 'delete-button') {
+        myLibrary['books'].splice(bookIndex, 1);
+      }
       displayBooks();
     });
-  }
-  // sample book:
-  // addBookToLibrary('The End of Eternity', 'Isaac Asimov', 250, true);
-
-  displayBooks();
-  setModalControls();
+    bookshelf.appendChild(book);
+  });
+  console.log(myLibrary);
 }
-main();
+displayBooks();
+
+// sets event listeners related to the 'add new book' modal
+function setModalControls() {
+  let modal = document.querySelector('.add-book-modal');
+  let form = document.querySelector('.add-book-form');
+
+  document.addEventListener('click', (event) => {
+    let button = event.target.className;
+    // if (button === 'new-book-button') modal.showModal();
+    if (button === 'close-button') {
+      form.reset();
+      modal.close();
+    }
+  });
+
+  // submit event is triggered by submit buttons when
+  // the associated form method is 'dialog'
+  document.addEventListener('submit', (event) => {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = +document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+    // addBookToLibrary(title, author, pages, read);
+    myLibrary.addBook(title, author, pages, read);
+    form.reset();
+    displayBooks();
+  });
+}
+setModalControls();
+// sample book:
+// addBookToLibrary('The End of Eternity', 'Isaac Asimov', 250, true);
+
+(function addMainWindowListeners() {
+  let modal = document.querySelector('.add-book-modal');
+  document.addEventListener('click', (event) => {
+    let button = event.target.className;
+    if (button === 'new-book-button') modal.showModal();
+  });
+})();
