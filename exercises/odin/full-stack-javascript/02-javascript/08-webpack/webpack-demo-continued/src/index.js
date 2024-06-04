5272456313323d6e06bd773bbfaa401627e473d5
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-06-04 23:28:40 +0900

    Update webpack demo with output mgmt.

--- exercises/odin/full-stack-javascript/02-javascript/08-webpack/webpack-demo-continued/src/index.js
+++ exercises/odin/full-stack-javascript/02-javascript/08-webpack/webpack-demo-continued/src/index.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.textContent = 'Click me!';
+  btn.onclick = printMe;
 
+  element.appendChild(btn);
   return element;
 }
 
--- exercises/odin/full-stack-javascript/02-javascript/08-webpack/webpack-demo-continued/webpack.config.js
+++ exercises/odin/full-stack-javascript/02-javascript/08-webpack/webpack-demo-continued/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: { index: './src/index.js', print: './src/print.js' },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
