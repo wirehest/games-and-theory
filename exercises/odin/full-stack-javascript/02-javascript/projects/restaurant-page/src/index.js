81cbd380396c97e573e6f38866cbc026e14d304e
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-06-08 06:24:37 +0900

    Fix responsive grids
    * Specifying height properties in rules for child and parent can
    cause problems, so had to re-do several of the properties.

--- exercises/odin/full-stack-javascript/02-javascript/projects/restaurant-page/src/index.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/restaurant-page/src/index.js
@@ -1,2 +1,5 @@
 import './style.css';
 import './assets/images/chicken-background.jpg';
+import './assets/images/icon-cart-arrow-down.svg';
+import './assets/images/icon-truck-delivery.svg';
+// import './assets/images/chicken-background.jpg';
