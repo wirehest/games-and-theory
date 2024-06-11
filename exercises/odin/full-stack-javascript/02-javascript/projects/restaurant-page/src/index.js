f825a8b35ada265c8be9add350e423776d9c501e
scoodivity <1033169+scoodivity@users.noreply.github.com>
2024-06-11 21:31:22 +0900

    Add menu page render function

--- exercises/odin/full-stack-javascript/02-javascript/projects/restaurant-page/src/index.js
+++ exercises/odin/full-stack-javascript/02-javascript/projects/restaurant-page/src/index.js
@@ -1,7 +1,20 @@
 import './style.css';
-import './assets/images/chicken-background.jpg';
 import './assets/images/icon-cart-arrow-down.svg';
 import './assets/images/icon-truck-delivery.svg';
+import mainBackground from './assets/images/chicken-background.jpg';
+import imageUrlWings from './assets/images/menu-chicken-wings.jpg';
+import imageUrlChixSandwich from './assets/images/menu-crispy-chix-sandwich.jpg';
+import imageUrlPulledSandwich from './assets/images/menu-pulled-sandwich.jpg';
+import imageUrlPopcorn from './assets/images/menu-popcorn-chicken.jpg';
+import imageUrlTenders from './assets/images/menu-chicken-tenders.jpg';
+import imageUrlDrumstick from './assets/images/menu-drumstick.jpg';
+import imageUrlSides from './assets/images/menu-sides.jpg';
+import imageUrlBeverages from './assets/images/menu-beverages.jpg';
+import imageUrlDessert from './assets/images/menu-dessert.jpg';
+
+let styles = getComputedStyle(document.documentElement);
+let breakpoint = styles.getPropertyValue('--breakpoint');
+let content = document.querySelector('#content');
 
 const main = `<div class="home-container">
       <section>
@@ -34,22 +47,66 @@ const main = `<div class="home-container">
 
 (function addNavListeners() {
   let header = document.querySelector('header');
+  let validButtons = ['menu', 'rewards', 'locations', 'catering', 'sign-in'];
   header.addEventListener('click', (event) => {
     let target = event.target;
-    // console.log(target.name);
-    if (target.className === 'header-middle') drawPage(main, true);
+
+    if (target.className === 'header-middle') {
+      clearPage();
+      drawPage(main);
+    }
+
+    if (validButtons.includes(target.name)) {
+      clearPage();
+      switch (target.name) {
+        case 'menu':
+          drawMenu();
+          break;
+      }
+    }
   });
 })();
 
-function drawPage(innerMarkup, background = false) {
-  let content = document.querySelector('#content');
-  let childNodes = content.childNodes;
-  console.log(childNodes);
-  content.innerHTML = innerMarkup;
-
-  if (background) {
-    content.classList.add('background');
+function clearPage() {
+  console.log(content.childNodes);
+  content.style.background = '';
+  for (let child of content.childNodes) {
+    content.removeChild(child);
   }
 }
 
-// drawPage(main, true);
+function drawPage(innerMarkup) {
+  content.style.background = `fixed center/cover url(${mainBackground})`;
+  content.innerHTML = innerMarkup;
+}
+
+function drawMenu() {
+  // TODO no bottom padding?
+  content.style.background = 'var(--color-window)';
+  let fragment = new DocumentFragment();
+  let menuItems = {
+    "Leeroy's Legendary Chicken": imageUrlWings,
+    'Buffalo Whelp Wings': imageUrlDrumstick,
+    "Majordomo's Chicken Sandwich": imageUrlChixSandwich,
+    'Pulled Pterrodax Sandwich': imageUrlPulledSandwich,
+    'Tinker Town Tenders': imageUrlTenders,
+    'Fel-Fried Popcorn Chicken': imageUrlPopcorn,
+    Sides: imageUrlSides,
+    Beverages: imageUrlBeverages,
+    Dessert: imageUrlDessert,
+  };
+  let menuContainer = document.createElement('div');
+  menuContainer.classList.add('menu-container');
+
+  for (let [name, image] of Object.entries(menuItems)) {
+    let menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+    menuItem.style.background = `top/contain no-repeat url(${image}) #fff`;
+    menuItem.textContent = name;
+    menuContainer.appendChild(menuItem);
+    fragment.appendChild(menuContainer);
+  }
+  content.appendChild(fragment);
+}
+
+drawPage(main);
