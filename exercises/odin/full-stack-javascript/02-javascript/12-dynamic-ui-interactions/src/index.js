9d4647ce3c73f74bd7e366a8e479ca6a90448822
multiplips <1033169+multiplips@users.noreply.github.com>
2024-08-06 00:19:56 +0900

    Add working left/right nav arrows

--- exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/index.js
+++ exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/index.js
@@ -3,3 +3,24 @@ import { Dropdown } from './classes.js';
 
 new Dropdown({ buttonSelector: '.menu-button1', menuSelector: '.menu1' });
 new Dropdown({ buttonSelector: '.menu-button2', menuSelector: '.menu2' });
+
+// setInterval(myCallback, 5000);
+
+function myCallback() {
+  console.log('tick');
+}
+
+let imageCarousel = document.querySelector('section.image-carousel');
+let carouselRoll = document.querySelector('.carousel-roll');
+
+imageCarousel.addEventListener('click', (e) => {
+  let className = e.target.className;
+
+  if (className === 'carousel-nav-left') {
+    carouselRoll.style.transform += 'translate(320px)';
+  }
+
+  if (className === 'carousel-nav-right') {
+    carouselRoll.style.transform += 'translate(-320px)';
+  }
+});
