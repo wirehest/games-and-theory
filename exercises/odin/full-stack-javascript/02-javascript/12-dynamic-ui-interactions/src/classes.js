203378d94fe5b4741901178a65bfcf0d8d13a19a
multiplips <1033169+multiplips@users.noreply.github.com>
2024-08-08 23:20:01 +0900

    Move HTML creation into Carousel class

--- exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/classes.js
+++ exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/classes.js
@@ -13,5 +13,133 @@ export class Dropdown {
 }
 
 export class ImageCarousel {
-  constructor() {}
+  rollMin = 1;
+  rollPosition = 1;
+
+  constructor(images, insertTarget, frameWidth) {
+    if (images.length === 0) {
+      this.images = [];
+    } else {
+      let sources = [...images];
+      sources.unshift(images.at(-1));
+      sources.push(images[0]);
+      this.images = sources;
+    }
+    this.insertTarget = insertTarget;
+    this.init();
+  }
+
+  // init remaining variables
+  init() {
+    this.imageCarousel = document.querySelector(this.insertTarget);
+    this.carouselRoll = document.querySelector(
+      `${this.insertTarget} .carousel-roll`,
+    );
+    this.rollLength = document.querySelectorAll(
+      `${this.insertTarget} .carousel-img`,
+    ).length;
+    this.rollMax = this.rollLength - 2;
+    this.makeCarouselFrame();
+    // console.log(this.images);
+    // console.log(this.carouselRoll);
+    // console.log(this.rollMax);
+  }
+
+  redrawCircles() {
+    //
+  }
+
+  makeCarouselFrame() {
+    let fragment = new DocumentFragment();
+
+    let divFrame = document.createElement('div');
+    divFrame.classList.add('carousel-frame');
+
+    let divArrowLeft = document.createElement('div');
+    divArrowLeft.classList.add('carousel-nav-left');
+
+    let divArrowRight = document.createElement('div');
+    divArrowRight.classList.add('carousel-nav-right');
+
+    let divCenter = document.createElement('div');
+    divCenter.classList.add('carousel-center');
+
+    let divRoll = document.createElement('div');
+    divRoll.classList.add('carousel-roll');
+
+    let divCircles = document.createElement('div');
+    divCircles.classList.add('carousel-nav-circles');
+
+    let ulCircles = document.createElement('ul');
+
+    for (let [i, image] of Object.entries(this.images)) {
+      let divImg = document.createElement('div');
+      divImg.classList.add('carousel-img');
+      divImg.textContent = image;
+
+      if (+i !== 0 && +i !== this.images.length - 1) {
+        let liCircle = document.createElement('li');
+        liCircle.classList.add('dot');
+        liCircle.setAttribute('data-imgnum', i);
+        if (+i === this.rollPosition) liCircle.classList.add('dot-filled');
+        ulCircles.append(liCircle);
+      }
+
+      divRoll.append(divImg);
+    }
+
+    divFrame.append(divArrowLeft, divCenter, divArrowRight);
+    divCenter.append(divRoll);
+    divCircles.append(ulCircles);
+    fragment.append(divFrame, divCircles);
+
+    this.imageCarousel.append(fragment);
+  }
 }
+
+/*
+imageCarousel.addEventListener('click', (e) => {
+  let className = e.target.className;
+
+  // console.log(className);
+  if (className === 'carousel-nav-right' || className === 'carousel-nav-left') {
+    let sign = className === 'carousel-nav-left' ? -1 : 1;
+    rollPosition = wrapPosition(rollPosition, sign, rollMin, rollMax);
+    let offset = -(rollPosition - 1) * 320;
+    carouselRoll.style.transform = `translate(${offset}px)`;
+  }
+});
+
+function wrapPosition(current, sign, minimum, maximum) {
+  current += sign;
+  if (current < minimum) current = maximum;
+  if (current > maximum) current = minimum;
+  return current;
+}
+
+
+<div class="carousel-frame">
+  <div class="carousel-nav-left"></div>
+  <div class="carousel-center">
+    <div class="carousel-roll">
+      <div class="carousel-img">5</div>
+      <div class="carousel-img">1</div>
+      <div class="carousel-img">2</div>
+      <div class="carousel-img">3</div>
+      <div class="carousel-img">4</div>
+      <div class="carousel-img">5</div>
+      <div class="carousel-img">1</div>
+    </div>
+  </div>
+  <div class="carousel-nav-right"></div>
+</div>
+<div class="carousel-nav-circles">
+  <ul>
+    <li class="dot"></li>
+    <li class="dot"></li>
+    <li class="dot"></li>
+    <li class="dot"></li>
+    <li class="dot"></li>
+  </ul>
+</div>
+*/
--- exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/index.js
+++ exercises/odin/full-stack-javascript/02-javascript/12-dynamic-ui-interactions/src/index.js
@@ -1,33 +1,42 @@
 import './style.css';
-import { Dropdown } from './classes.js';
+import { Dropdown, ImageCarousel } from './classes.js';
 
 new Dropdown({ buttonSelector: '.menu-button1', menuSelector: '.menu1' });
 new Dropdown({ buttonSelector: '.menu-button2', menuSelector: '.menu2' });
 
+let carousel = new ImageCarousel([1, 2, 3, 4, 5], 'section.image-carousel');
+
 let imageCarousel = document.querySelector('section.image-carousel');
 let carouselRoll = document.querySelector('.carousel-roll');
 
-let rollPosition = 1;
 let rollLength = document.querySelectorAll('.carousel-img').length;
 let rollMin = 1;
 let rollMax = rollLength - 2;
+let rollPosition = 1;
 
 imageCarousel.addEventListener('click', (e) => {
-  let className = e.target.className;
-  let sign = className === 'carousel-nav-left' ? -1 : 1;
+  let target = e.target;
+  let classList = [...target.classList];
+  let className = target.className;
+  let validNavClasses = ['carousel-nav-left', 'carousel-nav-right', 'dot'];
+  let offset;
 
-  if (className === 'carousel-nav-right') {
-    rollPosition = wrapPosition(++rollPosition, sign, rollMin, rollMax);
-    carouselRoll.style.transform = `translate(${-(rollPosition - 1) * 320}px)`;
-  }
+  if (validNavClasses.some((c) => classList.includes(c))) {
+    if (classList.includes('dot')) {
+      rollPosition = target.attributes['data-imgnum'].value;
+      // TODO redraw
+    } else {
+      let sign = className === 'carousel-nav-left' ? -1 : 1;
+      rollPosition = wrapPosition(rollPosition, sign, rollMin, rollMax);
+    }
 
-  if (className === 'carousel-nav-left') {
-    rollPosition = wrapPosition(--rollPosition, sign, rollMin, rollMax);
-    carouselRoll.style.transform = `translate(${-(rollPosition - 1) * 320}px)`;
+    offset = -(rollPosition - 1) * 320;
+    carouselRoll.style.transform = `translate(${offset}px)`;
   }
 });
 
 function wrapPosition(current, sign, minimum, maximum) {
+  current += sign;
   if (current < minimum) current = maximum;
   if (current > maximum) current = minimum;
   return current;
